692b5dbaced38c6be87902d5199431a4d0e6912a
walfrido-oliveira <walfrido_15@hotmail.com>
2022-04-24 15:23:05 -0300

    correção no db

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Schema::defaultStringLength(300);
+        Schema::defaultStringLength(191);
         \Carbon\Carbon::setLocale(config('app.locale'));
     }
 }
