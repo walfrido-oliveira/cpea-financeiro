35ce18e57283fa3576a9884dcb21c324f8023422
walfrido-oliveira <walfrido_15@hotmail.com>
2022-05-08 16:12:48 -0300

    #18

--- resources/views/formulas/scripts.blade.php
+++ resources/views/formulas/scripts.blade.php
@@ -16,4 +16,17 @@
         let classification = document.getElementById("accounting_classification_conditional_formula")
         textAtea.value += classification.value;
     });
+
+
+    document.querySelector(".btn-outline-success").addEventListener("click", function() {
+        let element1 = document.getElementById("accounting_classification_id");
+        let element2 = document.getElementById("type_classification");
+
+        element1.style.display = "block";
+        element1.style.position = "absolute";
+
+        element2.style.display = "block";
+        element2.style.position = "absolute";
+
+    });
 </script>
