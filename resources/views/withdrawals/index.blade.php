b65a0aae5a84b541a91278f0283a0081cb234d39
walfrido-oliveira <walfrido_15@hotmail.com>
2023-04-04 09:51:21 -0300

    Ajustes Módulo Financeiro #54

--- resources/views/withdrawals/index.blade.php
+++ resources/views/withdrawals/index.blade.php
@@ -32,11 +32,6 @@
                 <div class="mt-4 accounting-table">
                     <div class="flex items-center mx-4 my-2">
                         <h2>{{ __('Resultado do Exercício') }}</h2>
-                        <button type="button" class="btn-transition-primary px-2" id="btn_withdrawal_add" title="Adicionar nova classificação">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
-                            </svg>
-                        </button>
                     </div>
                     <div id="scroll_top_2" style="width: calc(100vw - 21rem); overflow: auto;">
                         <div style="height: 1px;"></div>
